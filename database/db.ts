fca7dfa01139cd1c5f68b480721399c8323d8b26
Damitha Perera <damithap@99x.io>
2023-12-13 10:47:23 +0530

    db change

--- database/db.ts
+++ database/db.ts
@@ -1,10 +1,11 @@
 import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: 'dataminr',
+    host: "mysql-5e6c904b-e85e-4bd2-9905-c21683402e28-dataminr3139456993-c.a.aivencloud.com",
+    user: "avnadmin",
+    password: "AVNS_Z8-SCAq9rtvIJJouEaA",
+    database: 'defaultdb',
+    port: 11716
 });
 console.log('⚡ DB connected')
 
